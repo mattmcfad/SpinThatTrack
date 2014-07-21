84f42d603eda06e995933a022ad8e093ce72eedd
Matt McFadyen <mmcfadyen91@gmail.com>
2014-07-21 16:22:28 -0400

    :ng: finished basic angular functionality :rocket:

--- dist/scripts/main.min.js
+++ dist/scripts/main.min.js
@@ -3,14 +3,10 @@
 		
 		app.controller('AlbumController',function(){
 			// Which album is selected and being displayed
-			this.selected = 0; 
+			this.selected = 0; //init to first one
 			
 			this.setSelected = function(newSelection){
 				this.selected = newSelection;
-			};
-
-			this.isSelected = function(selected){
-				return this.selected === selected;
 			};
 
 			this.music = music;
@@ -31,7 +27,6 @@
 		 */
 		var music = [
 			{
-				'id':1,
 				'artist':'Beatles',
 				'album': 'Abbey Road',
 				'imageUrlLarge':'images/albums/album-abbeyroad-large.jpg',
@@ -41,7 +36,6 @@
 				'isPurchased':false
 			},
 			{
-				'id':2,
 				'artist':'Bob Marley & The Wailers',
 				'album': 'Legend',
 				'imageUrlLarge':'images/albums/album-bobmarley-large.jpg',
@@ -50,8 +44,7 @@
 				'description':'The classic Marley album, the one that any fair-weather reggae fan owns, Legend contains 14 of his greatest songs. Some may argue that the compilation shortchanges his groundbreaking early ska work or his status as a political commentator, but this isn\'t meant to be definitive, it\'s meant to be an introduction, sampling the very best of his work. ',
 				'isPurchased':false
 			},
-			{	'id':3,
-				'artist':'Kanye West',
+			{	'artist':'Kanye West',
 				'album': 'Yeezus',
 				'imageUrlLarge':'images/albums/album-yeezus-large.jpg',
 				'imageUrlSmall':'images/albums/album-yeezus-small.jpg',
@@ -59,8 +52,7 @@
 				'description':'"Yeezus" is a medley of several genres -- new wave, punk, rock, and of course hip-hop. Those looking for vintage soul sounds or even full-on raps from start to finish will be thrown several curves here. Its an album with numerous emotional layers as well. There are a few lighthearted moments, and cuts about love along with lust.',
 				'isPurchased':false	
 			},
-			{	'id':4,
-				'artist':'Pink Floyd',
+			{	'artist':'Pink Floyd',
 				'album': 'The Dark Side of the Moon',
 				'imageUrlLarge':'images/albums/album-pinkfloyd-large.jpg',
 				'imageUrlSmall':'images/albums/album-pinkfloyd-small.jpg',
@@ -68,8 +60,7 @@
 				'description':'Dark Side of the Moon, was composed by Pink Floyd and was originally released on March 24 of 1973, a wonderful year to be alive. The hazy landscapes expressed therein are the perfect setting for perceiving the mundane details littered throughout life. With ten glistening movements including an overture, an elegy, and psychedelic and neo-baroque styles.',
 				'isPurchased':false	
 			},
-			{	'id':5,
-				'artist':'Jay-Z',
+			{	'artist':'Jay-Z',
 				'album': 'Magna Carta Holy Grail',
 				'imageUrlLarge':'images/albums/album-magnacarta-large.jpg',
 				'imageUrlSmall':'images/albums/album-magnacarta-small.jpg',
